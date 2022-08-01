86b8dbd37fce1e35f4777110bdda0a9eb4e1cfff
Aditya Dubey <adityadubey772@gmail.com>
2022-08-01 18:49:03 +0530

    Connecting with SQL Server and creating tables.

--- LMS.Web/Startup.cs
+++ LMS.Web/Startup.cs
@@ -1,3 +1,5 @@
+using LMS.Web.Data;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
@@ -22,6 +24,10 @@ namespace LMS.Web
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<ApplicationDbContext>((options) =>
+            {
+                options.UseSqlServer(Configuration.GetConnectionString("MyDefaultConnectionString"));
+            });
             services.AddRazorPages();
         }
 
