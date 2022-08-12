f8f0625d9ab94e9eec0a89e457d61c058d6da4c0
Aditya Dubey <adityadubey772@gmail.com>
2022-08-12 17:58:47 +0530

    Final

---
+++ LMS.xUnitTestProject/UnitTest1.cs
@@ -0,0 +1,36 @@
+using System;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace LMS.xUnitTestProject
+{
+    public class UnitTest1
+    {
+        private readonly ITestOutputHelper _testOutputHelper;
+
+        public UnitTest1(
+            ITestOutputHelper testOutputHelper)
+        {
+            _testOutputHelper = testOutputHelper;
+        }
+
+        [Fact]
+        public void Test1()
+        {
+            // ARRANGE
+            int expectedResult = 0;
+            int actualResult;
+            int a = 100, b = 5;
+
+            // ACT
+            actualResult = a % b;
+
+            _testOutputHelper.WriteLine($"input values are {a} and {b}");
+            _testOutputHelper.WriteLine("the modulo is {0}", a % b);
+            _testOutputHelper.WriteLine($"expectedResult = {expectedResult}, ActualResult = {actualResult}");
+
+            // ASSERT
+            Assert.Equal(expectedResult, actualResult);
+        }
+    }
+}
